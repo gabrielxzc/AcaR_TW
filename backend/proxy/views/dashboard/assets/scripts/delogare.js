a0f300a6ec405bb4d3571c359ad19210f7904496
stanciusebastian <sebastian_s97@yahoo.ro>
2018-06-20 01:07:11 +0300

    Stilizare

--- backend/proxy/views/dashboard/assets/scripts/delogare.js
+++ backend/proxy/views/dashboard/assets/scripts/delogare.js
@@ -1,5 +1,3 @@
-
-
 function Logout() {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:8079/logout");
@@ -8,19 +6,19 @@ function Logout() {
         let response = JSON.parse(xhr.response);
 
         if (response.status == "error") {
-            document.getElementById("myBtn2").style.backgroundColor = "#293742";
-            document.getElementById("myBtn2").innerHTML = "Logout!";
-        } else {
             document.getElementById("myBtn").style.backgroundColor="#293742";
-            document.getElementById("myBtn").innerHTML="Logout!"; 
-            window.location.replace("http://localhost:8079/");
+            document.getElementById("myBtn").innerHTML="Logout!"
+        } else {
+           document.getElementById("myBtn").style.backgroundColor="#293742";
+           document.getElementById("myBtn").innerHTML="Logout!"; 
+           window.location.replace("http://localhost:8079/"); 
         }
     });
     xhr.addEventListener("error", function errorCallback() {
         console.log("Network error");
     });
     document.getElementById("myBtn").style.backgroundColor="#293742";
-    document.getElementById("myBtn").innerHTML="Se proceseaza..."; 
+    document.getElementById("myBtn").style.innerHTML="Se proceseaza..";
     xhr.send();
 
 }
